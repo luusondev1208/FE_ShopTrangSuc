7915bff983858f712c2fdff82a83aff46ef0df7b
chien2k3 <106466627+chien2k3@users.noreply.github.com>
2023-11-30 11:16:31 +0700

    [FE] Fix Add Image

--- src/app/pages/admin/add-prodcut/add-prodcut.component.ts
+++ src/app/pages/admin/add-prodcut/add-prodcut.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from 'src/app/service/product.service';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FileUploader } from 'ng2-file-upload';
 import { NgxUploaderModule,UploaderOptions  } from 'ngx-uploader';
@@ -9,71 +9,56 @@ import { ToastrService } from 'ngx-toastr';
 import { CategoryService } from 'src/app/service/category.service';
 import { NgToastService } from 'ng-angular-popup';
 
+
 @Component({
   selector: 'app-add-prodcut',
   templateUrl: './add-prodcut.component.html',
   styleUrls: ['./add-prodcut.component.scss'],
 })
 export class AddProdcutComponent {
-  public files: File[] = [];
-  public images: File[] = [];
+  // public files: File[] = [];
+  // public images: File[] = [];
   categories: any = []
-  onFileChange(event: any) {
-    const files = event.target.files;
-  
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-  
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        // Lưu trữ thông tin đầy đủ của file, không chỉ là đường dẫn ảnh
-        const imageFile: File = file;
-        this.images.push(imageFile);
-  
-        // Bạn có thể sử dụng imageFile.name, imageFile.size, và các thuộc tính khác của File nếu cần
-      };
-  
-      reader.readAsDataURL(file);
-    }
-  }
+  productForm: FormGroup;
+  selectedImages: FileList | null = null;
   
 
   products: any = {};
-  productForm = this.formBuider.group({
-    title:[
-      '',
-      [Validators.required, Validators.minLength(6), Validators.maxLength(255)]
-    ],
-    price:[
-      0,
-      [Validators.required, Validators.min(1)]
-    ],
-    description:[
-      '',
-      [Validators.required, Validators.minLength(6), Validators.maxLength(255)]
-    ],
-    brand:[
-      '',
-      [Validators.required, Validators.minLength(6), Validators.maxLength(255)]
-    ],
-    slug:[
-      '',
-      [Validators.required, Validators.minLength(6), Validators.maxLength(255)]
-    ],
-    images:[
-      '',
-      [Validators.required]
-    ],
-    priceroot:[
-      '',
-      [Validators.required,Validators.min(1), Validators.minLength(6), Validators.maxLength(255)]
-    ],
-    category:[
-      '',
-      [Validators.required]
-    ],
+  // productForm = this.formBuider.group({
+  //   title:[
+  //     '',
+  //     [Validators.required, Validators.minLength(6), Validators.maxLength(255)]
+  //   ],
+  //   price:[
+  //     0,
+  //     [Validators.required, Validators.min(1)]
+  //   ],
+  //   description:[
+  //     '',
+  //     [Validators.required, Validators.minLength(6), Validators.maxLength(255)]
+  //   ],
+  //   brand:[
+  //     '',
+  //     [Validators.required, Validators.minLength(6), Validators.maxLength(255)]
+  //   ],
+  //   slug:[
+  //     '',
+  //     [Validators.required, Validators.minLength(6), Validators.maxLength(255)]
+  //   ],
+  //   images:[
+  //     '',
+  //     [Validators.required]
+  //   ],
+  //   priceroot:[
+  //     '',
+  //     [Validators.required,Validators.min(1), Validators.minLength(6), Validators.maxLength(255)]
+  //   ],
+  //   category:[
+  //     '',
+  //     [Validators.required]
+  //   ],
     
-  })
+  // })
   constructor(private productService: ProductService, private categoryService: CategoryService, private router: Router, private formBuider: FormBuilder, private toast: NgToastService) {
     this.categoryService.getCategories().subscribe(
       (response:any) => {
@@ -83,27 +68,54 @@ export class AddProdcutComponent {
         console.log(error);
       }
     );
+    this.productForm = this.formBuider.group({
+      title: [''],
+      description: [''],
+      brand: [''],
+      price: [0],
+      // ... other form fields ...
+    });
 
   }
-  onsubmit() {
-    this.productService.addProducts(this.products, this.files).subscribe((response) => {
-      console.log('san pham them thanh cong: ',response);
-      
-      this.toast.success({ detail: "Thông báo", summary: `add thành công: ${response.createdProduct.title}`, duration: 5000, position: "topRight" });
-      this.router.navigate(['/admin/list'])
-      // alert("add thanh cong")
-      // Thực hiện các hành động sau khi sản phẩm được thêm thành công
-    },
-    error => {
-      this.toast.error({ detail: "Thông báo", summary: 'Lỗi khi thêm sản phẩm!', duration: 5000, position: "topRight" });
-      console.log("loi khi them san pham: ", error);
-      // Xử lý lỗi nếu có
+  ngOnInit(): void {
+    this.productForm = this.formBuider.group({
+      title: ['', [Validators.required]],
+      priceroot: ['', [Validators.required, Validators.min(0)]],
+      price: ['', [Validators.required, Validators.min(0)]],
+      images: ['', [Validators.required]], // Add the required validator for images
+      brand: ['TTL'], // Set a default value or remove it if not needed
+      description: ['', [Validators.required, Validators.minLength(6)]],
+      category: ['', [Validators.required]],
+    });
+  }
+  onFilesSelected(event: any) {
+    this.selectedImages = event.target.files;
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('title', this.productForm.value.title);
+    formData.append('description', this.productForm.value.description);
+    formData.append('brand', this.productForm.value.brand);
+    formData.append('price', this.productForm.value.price);
+    // ... append other form fields ...
+
+    if (this.selectedImages) {
+      for (let i = 0; i < this.selectedImages.length; i++) {
+        formData.append('image', this.selectedImages[i]);
+      }
     }
+
+
+    this.productService.addProduct(formData).subscribe(
+      (response) => {
+        console.log('Product added successfully', response);
+      },
+      (error) => {
+        console.error('Error adding product', error);
+      }
     );
-    console.log('Danh sách ảnh:', this.images);
   }
-
-
   // onFileDropped(event: any) {
   //   if (event instanceof FileList) {
   //     for (let i = 0; i < event.length; i++) {

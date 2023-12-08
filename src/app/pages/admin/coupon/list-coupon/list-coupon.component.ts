9f4caf81fbebc202e462b10ad2b969b12a1c1722
LuuSohn <luuhoangson2002@gmail.com>
2023-12-09 00:05:43 +0700

    [FE] New Foder Coupon and Brand

---
+++ src/app/pages/admin/coupon/list-coupon/list-coupon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-coupon',
+  templateUrl: './list-coupon.component.html',
+  styleUrls: ['./list-coupon.component.scss']
+})
+export class ListCouponComponent {
+
+}
---
+++ src/app/pages/admin/coupon/update-coupon/update-coupon.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UpdateCouponComponent } from './update-coupon.component';
+
+describe('UpdateCouponComponent', () => {
+  let component: UpdateCouponComponent;
+  let fixture: ComponentFixture<UpdateCouponComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [UpdateCouponComponent]
+    });
+    fixture = TestBed.createComponent(UpdateCouponComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

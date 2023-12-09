3aa8c926463b471210de8a5a08175ecb49f2cca9
Kiendev99 <xinchaoace9@gmail.com>
2023-12-09 15:15:11 +0700

    Order, lsđh fix

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -20,6 +20,14 @@ export class OrderComponent {
   userLogin: any
   paymentMethodChecked: string = ""
   urlPayment: any = ""
+  orderData: any = {
+    name: '',
+    mobile: '',
+    address: '',
+    user: '',
+    note: '',
+    email: '',
+  };
 
 
   constructor(private cartService: CartService,
@@ -90,25 +98,23 @@ export class OrderComponent {
       return;
     }
 
-    let data: any = {
-      user: this.userLogin._id,
-      totalPrice: this.calculatePrice(),
-      paymentMethod: this.paymentMethodChecked,
-    };
+    this.orderData.user = this.userLogin._id;
+    this.orderData.totalPrice = this.calculatePrice();
+    this.orderData.paymentMethod = this.paymentMethodChecked;
 
     if (this.paymentMethodChecked === "Thanh toán khi nhận hàng" && this.checkedVoucher) {
-      data.vouchers = [this.checkedVoucher._id];
+      this.orderData.vouchers = [this.checkedVoucher._id];
     }
 
     if (this.paymentMethodChecked === "Thanh toán khi nhận hàng") {
-      this.orderSevice.createOrder(data).subscribe((response) => {
+      this.orderSevice.createOrder(this.orderData).subscribe((response) => {
         alert(response.message);
       }, (error) => {
         alert(error.error.message)
       })
 
     } else {
-      this.orderSevice.createOrder(data).subscribe((response) => {
+      this.orderSevice.createOrder(this.orderData).subscribe((response) => {
         const paymentData = {
           amount: this.calculatePrice(),
           language: "vn"
@@ -118,11 +124,13 @@ export class OrderComponent {
         localStorage.setItem("idOrder", response.order._id)
 
         this.orderSevice.createPaymentUrl(paymentData).subscribe((res) => {
+
           window.location.href = res.url
 
         }, (err) => {
           console.log(err);
         })
+
       }, (error) => {
         alert(error.error.message)
       })
@@ -149,7 +157,7 @@ export class OrderComponent {
     const local = localStorage.getItem("user");
     const user: any = local && JSON.parse(local);
     this.userLogin = user;
-    this.userService.getUser(user._id).subscribe((res) => { // đây
+    this.userService.getUser(user._id).subscribe((res) => {
       this.cartService.getCart(res.use.cart).subscribe((items: any) => {
         this.cartItems = items?.cart;
         this.onSubmit()

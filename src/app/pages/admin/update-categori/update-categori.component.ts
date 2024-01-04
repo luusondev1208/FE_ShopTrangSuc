a703dcaac7424649543fdc0971d24c2a10959193
luongph28185 <luongttph28185@fpt.edu.vn>
2024-01-04 15:49:46 +0700

    update table

--- src/app/pages/admin/update-categori/update-categori.component.ts
+++ src/app/pages/admin/update-categori/update-categori.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
 import { CategoryService } from 'src/app/service/category.service';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-update-categori',
@@ -13,51 +14,52 @@ export class UpdateCategoriComponent {
   category!:any;
   title: any
   categoryForm = this.formBuilder.group({
-   
+    id:[
+      '',
+    ],
     title:[
       '',
       [Validators.required, Validators.minLength(6), Validators.maxLength(255)]
     ]
   });
+  @Input() data: any; // Thuộc tính để nhận dữ liệu từ bên ngoài
   constructor(
+    // @Inject(MAT_DIALOG_DATA) public data: any,
     private formBuilder: FormBuilder,
     private categoryService: CategoryService,
     private router: Router,
     private route: ActivatedRoute,
     private toast: NgToastService
   ){
-    this.route.paramMap.subscribe((param)=> {
-      const id = String(param.get('id'))
-      this.categoryService.getCategory(id).subscribe(
-        (category) => {
-          // console.log(category);
-          this.category = category.getOneCategory;
-          this.categoryForm.patchValue({
-            title: category.getOneCategory.title,
-          })
+    
+    // this.route.paramMap.subscribe((param)=> {
+    //   const id = String(param.get('id'))
+    //   this.categoryService.getCategory(id).subscribe(
+    //     (category) => {
+    //       this.category = category.getOneCategory;
+    //       this.categoryForm.patchValue({
+    //         title: category.getOneCategory.title,
+    //       })
           
-        },
-        // (error) => console.log(error.message)
-      );
-    });
+    //     },
+    //   );
+    // });
   }
+  // Gán giá trị từ data vào FormControl trong FormGroup
+  
   onHandleSubmit() {
     if (this.categoryForm.valid) {
-      const category = {
-        id: this.category._id,
-        title: this.categoryForm.value.title || '',
-       
-        
-      };
-      
-
-      this.categoryService.updateCategory(category).subscribe((category) => {
-        // console.log(category);
-        
-        this.title = category.updateCategory.title;
-        this.toast.success({ detail: "Thông báo", summary: `Sửa thành công, danh mục mới: ${this.title}`, duration: 5000, position: "topRight" });
-        this.router.navigate(['/admin/listCategori']);
-      });
+      const updatedData = { ...this.categoryForm.value, };
+      this.categoryService.updateCategory(updatedData).subscribe(
+        (response) => {
+          console.log('Update successful:', response);
+          // Xử lý sau khi cập nhật thành công, ví dụ: điều hướng tới trang khác
+          this.router.navigate(['/your-route']);
+        },
+        (error) => {
+          console.error('Update failed:', error);
+        }
+      );
     }
   }
 }
